baa66bfc3d7ca494aaf6dbc478fcbaafc42de842
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2025-02-13 10:15:09 -0600

    Update Utils.swift

--- Pod/Classes/Utils.swift
+++ Pod/Classes/Utils.swift
@@ -2,7 +2,11 @@ import Foundation
 
 extension URL {
 	func bundleLikeURL(forResource: String, withExtension: String?) -> URL? { 
-		let url = self.appending(component: forResource).appendingPathExtension(withExtension ?? "")
+		if #available(iOS 16.0, *) {
+			let url = self.appending(component: forResource).appendingPathExtension(withExtension ?? "")
+		} else {
+			let url = self.appendingPathComponent(forResource).appendingPathExtension(withExtension ?? "")
+		}
 		guard FileManager.default.fileExists(atPath: url.path()) else {
 			return nil
 		}
